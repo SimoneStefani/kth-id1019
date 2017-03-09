f04ed65c3b0f458f11482e875d1d9a565f1b516c
Simone Stefani <s.stefani95@gmail.com>
2017-03-09 21:14:22 +0100

    Implement basic insert for 2-3 tree

--- exercises/2-3-tree.erl
+++ exercises/2-3-tree.erl
@@ -17,4 +17,19 @@
 % a two-node: {two, Key, Left, Right}
 % a three-node: {three, K1, K2, Left, Middle, Right}
 
+% Temporary:
 % a four-node: {four, K1, K2, K3, Left, M1, M2, Right}
+
+
+%%%-------------------------------------------------------------------
+%%% Insertion
+%%%-------------------------------------------------------------------
+
+insert(Key, Value, nil) -> {leaf, Key, Value};
+
+insert(Key, Value, {leaf, K, _}=L) ->
+  if
+    Key <= K -> {two, Key, {leaf, Key, Value}, L};
+    true -> {two, K, L, {leaf, Key, Value}};
+  end.
+
